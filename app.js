bcd8f84c9ac0d656fb93e6e532794dca66a5f3ff
big-silver <danielsstar89@gmail.com>
2018-04-15 03:43:58 +0200

    implement the change password

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var moment = require('moment');
+var jwt    = require('jsonwebtoken'); 
 const xoauth2 = require('xoauth2');
 
 var indexRouter = require('./routes/index');
@@ -52,6 +53,7 @@ db.once('open', function() {
   console.log('Connected successfully to server')
 });
 
+app.set('superSecret', db_config.secret);
 
 var user = mongoose.model('users');
 var generalMessage = mongoose.model('generalMessages');
@@ -72,7 +74,21 @@ app.post('/login', (req, res, next) => {
       next(error); 
     }
     if (user[0]) {
-      res.send(user);
+      const payload = {
+        id: user[0]._id 
+      };
+      var token = jwt.sign(payload, app.get('superSecret'), {
+        expiresIn: 1440
+      });
+      var verified_user = {
+        _id : user[0]._id,
+        name: user[0].name,
+        workspace: user[0].workspace,
+        email: user[0].email,
+        password: user[0].password,
+        token: token
+      }
+      res.send(verified_user);
     } else {
       error.name = 'BadRequest';
       error.message = 'User does not exists.';
@@ -118,13 +134,71 @@ app.post('/register', (req, res, next) => {
             ws[0].set({users: updated_users});
             ws[0].save(function (err, updated_ws) {
               if (err) next(err);
-              res.send(user);            
+              var token = jwt.sign(payload, app.get('superSecret'), {
+                expiresIn: 1440
+              });
+              var verified_user = {
+                _id : user._id,
+                name: user.name,
+                workspace: user.workspace,
+                email: user.email,
+                password: user.password,
+                token: token
+              }
+              res.send(verified_user);            
             })
           }
         })
       });
     }
   })
+});
+
+app.post('/changepassword', (req, res, next) => {
+  var error = new Error();
+  var change_user = {
+    workspace: req.body.workspaceId,
+    email: req.body.email,
+    password: req.body.password
+  }
+  user.find(change_user, function (err, user) {
+    if (err) {
+      error.name = 'BadRequest';
+      error.message = 'Changepassword is failed.';
+      error.status = 400;
+      next(error); 
+    }
+    if (user[0]) {
+      user[0].password = req.body.newPassword;
+      user[0].save(function (err, updated_user) {
+        if (err) {
+          error.name = 'BadRequest';
+          error.message = 'Changepassword is failed.';
+          error.status = 400;
+          next(error);
+        }
+        res.send(updated_user);
+      })
+    } else {
+      error.name = 'BadRequest';
+      error.message = 'User does not exists.';
+      error.status = 404;
+      next(error); 
+    }
+  })
+})
+
+app.get('/me', (req, res, next) => {
+  var error = new Error();
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  
+  jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    
+    res.status(200).send(decoded);
+  });
 });
 
 app.get('/reset', (req, res, next) => {  
--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
+	'secret': 'bigsilverseniorweb',
 	url : 'mongodb://users:user1992@ds157723.mlab.com:57723/userauth'
 }
