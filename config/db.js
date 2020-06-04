b8ead1fcdab89bd8f5be437ecd36b28852a47ff0
Senior Software Engineer <bigsilversstar@yandex.com>
2020-06-04 08:37:32 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'secret': 'bigsilverseniorweb',
-	url : 'mongodb://users:user1992@ds157723.mlab.com:57723/userauth'
+	'secret': 'XXXXXXXXXXXX',
+	url : 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 }
